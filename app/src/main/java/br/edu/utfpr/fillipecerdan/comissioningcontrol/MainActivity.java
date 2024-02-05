5fe178df73a71ef1b01994176a754911315c6602
Cerdan <fillipe.cerdan@gmail.com>
2024-02-05 18:01:04 -0300

    First Commit

---
+++ app/src/main/java/br/edu/utfpr/fillipecerdan/comissioningcontrol/MainActivity.java
@@ -0,0 +1,14 @@
+package br.edu.utfpr.fillipecerdan.comissioningcontrol;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

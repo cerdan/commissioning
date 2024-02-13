c68dda85551536f035b0b7f924109fe0e48165d3
Cerdan <fillipe.cerdan@gmail.com>
2024-02-13 17:00:13 -0300

    Log wrapper added

--- app/src/main/java/br/edu/utfpr/fillipecerdan/comissioningcontrol/utils/Misc.java
+++ app/src/main/java/br/edu/utfpr/fillipecerdan/comissioningcontrol/utils/Misc.java
@@ -1,11 +1,16 @@
 package br.edu.utfpr.fillipecerdan.comissioningcontrol.utils;
 
+import android.util.Log;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
 public class Misc {
+    public static final String KEY_EQUIPMENT = "equipment";
+    public static final String TAG_FOR_LOG = "Fillipe";
+
     public static Date parseDate(String date, SimpleDateFormat formatter) {
         try {
             return formatter.parse(date);
@@ -16,5 +21,9 @@ public class Misc {
     public static Date parseDate(String date) {
         return parseDate(date, new SimpleDateFormat("dd-MM-yyyy hh:mm:ss a", Locale.ENGLISH));
 
+    }
+
+    public static void log(String msg){
+        Log.w(TAG_FOR_LOG, msg);
     }
 }

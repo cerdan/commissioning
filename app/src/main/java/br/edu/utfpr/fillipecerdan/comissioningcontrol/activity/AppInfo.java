cd984e8dffd706fdc6f91ecd72726f4b20a4cc35
Cerdan <fillipe.cerdan@gmail.com>
2024-02-13 19:43:43 -0300

    App info activity created

---
+++ app/src/main/java/br/edu/utfpr/fillipecerdan/comissioningcontrol/activity/AppInfo.java
@@ -0,0 +1,16 @@
+package br.edu.utfpr.fillipecerdan.comissioningcontrol.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import br.edu.utfpr.fillipecerdan.comissioningcontrol.R;
+
+public class AppInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_info);
+    }
+}

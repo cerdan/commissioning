3c9b039c372510acd39527ef7531635c43b5c126
Cerdan <fillipe.cerdan@gmail.com>
2024-02-16 15:59:33 -0300

    AppInfoActivity now has a button up

--- app/src/main/java/br/edu/utfpr/fillipecerdan/comissioningcontrol/activity/AppInfoActivity.java
+++ app/src/main/java/br/edu/utfpr/fillipecerdan/comissioningcontrol/activity/AppInfoActivity.java
@@ -2,9 +2,11 @@ package br.edu.utfpr.fillipecerdan.comissioningcontrol.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import br.edu.utfpr.fillipecerdan.comissioningcontrol.R;
@@ -16,7 +18,20 @@ public class AppInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_info);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) actionBar.setDisplayHomeAsUpEnabled(true);
+
         setTitle(getResources().getString(R.string.lblStringAbout));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == android.R.id.home){
+            finishMe(null);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void finishMe(View view){

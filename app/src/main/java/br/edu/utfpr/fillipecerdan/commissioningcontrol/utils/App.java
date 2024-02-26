522f8ac11ec22b9801ad910b215b662b8fa64295
Cerdan <fillipe.cerdan@gmail.com>
2024-02-26 18:32:57 -0300

    New constants

--- app/src/main/java/br/edu/utfpr/fillipecerdan/commissioningcontrol/utils/App.java
+++ app/src/main/java/br/edu/utfpr/fillipecerdan/commissioningcontrol/utils/App.java
@@ -8,14 +8,19 @@ import br.edu.utfpr.fillipecerdan.commissioningcontrol.model.EquipmentType;
 public class App extends Application {
 
     public static final String KEY_EQUIPMENT = "EQUIPMENT";
+    public static final String KEY_PROJECT = "PROJECT";
     public static final String KEY_RENAME= "LAST_NAME";
     public static final String PREFERENCES = "br.edu.utfpr.fillipecerdan.commissioningcontrol.PREFERENCES";
-    public static final String KEY_PREF_ORDER= "ORDER";
+    public static final String KEY_PREF_ORDER_EQUIPMENT = "ORDER_EQUIPMENT";
+    public static final String KEY_PREF_ORDER_PROJECT = "ORDER_PROJECT";
     public static final int PREF_ORDER_DEFAULT= 0;
     public static final int PREF_ORDER_TAG_ONLY = 1;
     public static final int PREF_ORDER_NOK_FIRST= 2;
     public static final int PREF_ORDER_OK_FIRST= 3;
     public static final int PREF_ORDER_LAST_CHANGE = 4;
+    public static final int PREF_ORDER_PRJ_CUSTOMER = 1;
+    public static final int PREF_ORDER_PRJ_LOCATION = 2;
+    public static final int PREF_ORDER_PRJ_YEAR = 3;
     public static final String KEY_PREF_SUGGEST_TYPE = "SUGGEST_TYPE";
     public static final String KEY_PREF_LAST_TYPE = "LAST_TYPE";
     public static final boolean PREF_SUGGEST_TYPE_DEFAULT = true;
